255d90fbab598ac07793b2d227edc977923bf065
Yeonjun Lee <yeonfish6040@gmail.com>
2024-09-19 07:52:36 +0900

    Language support

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-const SearchMethodValues = ["koko"] as const;
-type SearchMethod = (typeof SearchMethodValues)[number];
+const SearchTypeValues = ["koko", "enko", "ccko", "jako", "zhko", "frko", "esko", "deko"] as const;
+type SearchType = (typeof SearchTypeValues)[number];
 
 /**
  * @Method: Returns the means of query
  * @param { string } query - search query
- * @param { SearchMethod } type - search type ex) koko, enen
+ * @param { SearchType } type - search type ex) koko, enen
  * @Return {string}
  */
-export async function search(query: string, type: SearchMethod) {
-    if (query === undefined || SearchMethodValues.indexOf(type) === -1)
+export async function search(query: string, type: SearchType) {
+    if (query === undefined || SearchTypeValues.indexOf(type) === -1)
         throw new Error("Incorrect parameters");
 
     const wordUUIDSource = `https://ac-dict.naver.com/${type}/ac?st=1&r_lt=1&q=${query}`;
